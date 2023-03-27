9ddffa6793f46e8290aac91541ddf22c92156988
madhaviaws <madhavi.oct14@gmail.com>
2023-03-27 12:49:55 +0530

    tf vpc

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.VPC_CIDR
+  enable_dns_hostnames = true
+
+tags = {
+    Name = "${var.ENV}-vpc"
+  }
 }
+
+#enable_dns_hostnames = true gives  public DNS hostnames
